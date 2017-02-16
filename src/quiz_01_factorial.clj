43133b3d5315b7adc2c0cb5b0f657d8b6d48c720
Erich Grunewald <erich.grunewald@futurice.com>
2017-02-16 18:25:58 +0100

    Rename 01_factorial.clj -> quiz_01_factorial.clj and improve documentation.

--- src/quiz_01_factorial.clj
+++ src/quiz_01_factorial.clj
@@ -1,3 +1,6 @@
+;;; This quiz is not so much a quiz as a simple exercise -- all we want you to do
+;;; is to implement the factorial function.
+
 ;; Here we define our namespace -- quiz-01-factorial -- and import the testing
 ;; library (which is among the core libraries).
 (ns quiz-01-factorial
@@ -5,6 +8,10 @@
 
 ;; This is the factorial function. Right now, it doesn't work; it's up to you to
 ;; implement it in order to get the tests passing.
+;;
+;; In order to implement it correctly, you'll probably want to know something
+;; about Clojure's "if" expression (https://clojuredocs.org/clojure.core/if) and
+;; recursion.
 (defn factorial [x]
   x)
 
@@ -26,3 +33,12 @@
 (deftest factorial-test
   (is (= 1 (factorial 0)))
   (is (= 120 (factorial 5))))
+
+;; In order to run the project in the REPL from the command line, do:
+;;
+;;    lein repl
+;;
+;; and then, in the REPL, load the file and run the tests like so:
+;;
+;;    (require 'quiz-01-factorial)
+;;    ???
