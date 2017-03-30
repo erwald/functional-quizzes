c7446f40fe2686b4401ef20621b199fbb26dd4fa
Julius Peinelt <jpeinelt@users.noreply.github.com>
2017-03-30 14:37:13 +0200

    Added a solution for the reverse polish notation calculator.
    This solution uses partial, drop, take, reduce and a Map.

---
+++ src/quiz_06_reverse_polish.clj
@@ -0,0 +1,34 @@
+(ns quiz-06-reverse-polish
+(:require [clojure.test :refer :all]))
+
+
+(defn rpn [expression]
+  "Calculates the RPN expression.
+   An argument could be '(3 5 + 2 *)..."
+  (parse-and-apply expression '()))
+
+(defn parse-and-apply [expr stack]
+  "Parses the elements of expr and puts them on the stack.
+   If the element is an operator, it applies it to the stack."
+  (cond
+    (empty? expr) (first stack)
+    :else (let [fst (first expr)]
+            (if (contains? #{'+ '- '* '/} fst)
+              (parse-and-apply
+                (rest expr)
+                (conj (drop 2 stack) (reduce (apply-op fst) (take 2 stack))))
+              (parse-and-apply (rest expr) (conj stack fst))))))
+
+
+(defn apply-op [op]
+  "Returns a partial function for a quoted operator (like '+)."
+  (cond
+    (= op '+) (partial +)
+    (= op '-) (partial -)
+    (= op '*) (partial *)
+    (= op '/) (partial /)))
+
+;; some simple tests
+(rpn '(1 1 +))
+(rpn '(2 3 * 5 +))
+(rpn '(2 3 5 * +))
